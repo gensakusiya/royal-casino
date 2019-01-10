e0a978ad441b31d58cfbdf5f1e6992cb49346763
Artyom Prikhodko <homoogenecus@gmail.com>
2019-01-10 18:00:35 +0300

    test build

---
+++ docs/precache-manifest.d891d3ce2828847232266172c8d95622.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4ea2d47a9d0567b7442b",
+    "url": "/static/js/main.4ea2d47a.chunk.js"
+  },
+  {
+    "revision": "5866fed0469f991ffbb4",
+    "url": "/static/js/1.5866fed0.chunk.js"
+  },
+  {
+    "revision": "4ea2d47a9d0567b7442b",
+    "url": "/static/css/main.a50b7038.chunk.css"
+  },
+  {
+    "revision": "5866fed0469f991ffbb4",
+    "url": "/static/css/1.4fbee686.chunk.css"
+  },
+  {
+    "revision": "1ea52bb3581368511e2527e44cef553a",
+    "url": "/index.html"
+  }
+];
