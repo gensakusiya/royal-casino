cf9ba6c6d7cac4a59aae47862244ee85d9923125
Artyom Prikhodko <homoogenecus@gmail.com>
2019-01-10 18:08:38 +0300

    Updates

---
+++ precache-manifest.9efd7fa2f3d88f19786aadb5df642844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "427a567fd45174debcb5",
+    "url": "/royal-casino/static/js/runtime~main.427a567f.js"
+  },
+  {
+    "revision": "4ea2d47a9d0567b7442b",
+    "url": "/royal-casino/static/js/main.4ea2d47a.chunk.js"
+  },
+  {
+    "revision": "c6573af11b619aecba70",
+    "url": "/royal-casino/static/js/1.c6573af1.chunk.js"
+  },
+  {
+    "revision": "4ea2d47a9d0567b7442b",
+    "url": "/royal-casino/static/css/main.a50b7038.chunk.css"
+  },
+  {
+    "revision": "c6573af11b619aecba70",
+    "url": "/royal-casino/static/css/1.4fbee686.chunk.css"
+  },
+  {
+    "revision": "b7442d8e90a4acbb080dfe738caafa86",
+    "url": "/royal-casino/index.html"
+  }
+];
